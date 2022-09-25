52f585080cbdaed5fb7457b047badaf5d5ba0ff9
hitham <somekms@hotmail.com>
2022-09-25 11:27:54 +0200

    rep2

---
+++ HealthServicesSystem/Reports/RPTِEstrdadCountLoc.cs
@@ -0,0 +1,42 @@
+namespace MedicalServiceSystem.Reports
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Telerik.Reporting;
+    using Telerik.Reporting.Drawing;
+
+    /// <summary>
+    /// Summary description for RPTChronicBooksDetails.
+    /// </summary>
+    public partial class RPTöEstrdadCountLoc : Telerik.Reporting.Report
+    {
+        public RPTöEstrdadCountLoc()
+        {
+            //
+            // Required for telerik Reporting designer support
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+
+        private void RPTöEstrdadCountLoc_ItemDataBinding(object sender, EventArgs e)
+        {
+        }
+
+        private void TextBox17_ItemDataBound(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void RPTöEstrdadCountLoc_ItemDataBound(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
