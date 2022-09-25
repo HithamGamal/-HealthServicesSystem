690630f671d7fb55cced17ddb05b5e4bb94592ed
hitham <somekms@hotmail.com>
2022-09-25 11:19:37 +0200

    app

---
+++ HealthServicesSystem/ReportForAll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using ModelDB;
+namespace MedicalServiceSystem
+{
+	public class ReportForAll
+    {		
+		public int? Row1 { get; set; }
+        public decimal? Row2 { get; set; }
+        public decimal? Row3 { get; set; }
+        public int? Row4 { get; set; }
+        public int? Row5 { get; set; }
+        public string Row6 { get; set; }
+		public string Row7 { get; set; }
+        public string Row8 { get; set; }
+        public string Row9 { get; set; }
+        public string Row10 { get; set; }
+        public decimal? Row11 { get; set; }
+		public decimal? Row12 { get; set; }
+        public decimal? Row21 { get; set; }
+        public decimal? Row22 { get; set; }
+        public DateTime? Row13 { get; set; }
+        public DateTime? Row14 { get; set; }
+        public string Row15 { get; set; }
+        public string Row16 { get; set; }
+        public string Row17 { get; set; }
+        public string Row18 { get; set; }
+        public string Row19 { get; set; }
+        public string Row20 { get; set; }
+
+    }
+
+}
+
