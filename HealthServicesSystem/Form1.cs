1ef8aa04dfbe61fee691dae88e6e6fcb02077bf2
Doha <Doha@DESKTOP-3R1LKG9>
2022-11-07 06:45:41 +0200

    .

--- HealthServicesSystem/Form1.cs
+++ HealthServicesSystem/Form1.cs
@@ -19,7 +19,7 @@ namespace HealthServicesSystem
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test")
+            MessageBox.Show("Test");
         }
     }
 }
