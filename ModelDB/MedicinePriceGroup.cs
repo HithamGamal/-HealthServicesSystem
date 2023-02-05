bc2c874425d89453323129546140218775e4738a
Saif <saif.rashid1974@gmail.com>
2023-02-05 09:15:27 +0200

    saif_2023_2

---
+++ ModelDB/MedicinePriceGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ModelDB
+{
+	public class MedicinePriceGroup
+    {
+
+		public int Id { get; set; }
+		public string GroupNameName { get; set; }
+
+        public decimal GroupMaxGroup  { get; set; }
+		public bool IsEnabled { get; set; }
+      
+	}
+}
