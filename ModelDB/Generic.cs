ae2662038a384b42e338a1df973e492a96f0a8d8
Saif <saif.rashid1974@gmail.com>
2023-02-06 08:42:10 +0200

    saif_2023_2_6_1

--- ModelDB/Generic.cs
+++ ModelDB/Generic.cs
@@ -8,9 +8,6 @@ namespace ModelDB
       
         public int Id { get; set; }
         public string GenericName { get; set; }
-        public int Unit_Id { get; set; }
-        [ForeignKey("Unit_Id")]
-        public virtual Unit Unit { get; set; }
         public int IsActive { get; set; }
     }
 
