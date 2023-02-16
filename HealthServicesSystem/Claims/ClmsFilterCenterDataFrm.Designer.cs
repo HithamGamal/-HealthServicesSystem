4a4f6062cc2d6ffa68580461f13fec37e09322ae
hitham <somekms@hotmail.com>
2023-02-16 08:07:07 +0200

    22555

---
+++ HealthServicesSystem/Claims/ClmsFilterCenterDataFrm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace HealthServicesSystem.Claims
+{
+    partial class ClmsFilterCenterDataFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn1 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn2 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn3 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn4 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn5 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn6 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn7 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn8 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn9 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn10 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn11 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn12 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn13 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn14 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.radGridView2 = new Telerik.WinControls.UI.RadGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView2.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGridView2
+            // 
+            this.radGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radGridView2.Location = new System.Drawing.Point(0, 0);
+            // 
+            // 
+            // 
+            this.radGridView2.MasterTemplate.AllowAddNewRow = false;
+            this.radGridView2.MasterTemplate.AllowDeleteRow = false;
+            gridViewTextBoxColumn1.FieldName = "Id";
+            gridViewTextBoxColumn1.HeaderText = "رقم الروشتة";
+            gridViewTextBoxColumn1.Name = "Id";
+            gridViewTextBoxColumn1.Width = 100;
+            gridViewTextBoxColumn2.EnableExpressionEditor = false;
+            gridViewTextBoxColumn2.FieldName = "Months";
+            gridViewTextBoxColumn2.HeaderText = "الشهر";
+            gridViewTextBoxColumn2.Name = "Months";
+            gridViewTextBoxColumn2.Width = 80;
+            gridViewTextBoxColumn3.EnableExpressionEditor = false;
+            gridViewTextBoxColumn3.FieldName = "Years";
+            gridViewTextBoxColumn3.HeaderText = "السنة";
+            gridViewTextBoxColumn3.Name = "Years";
+            gridViewTextBoxColumn3.Width = 80;
+            gridViewTextBoxColumn4.FieldName = "PatName";
+            gridViewTextBoxColumn4.HeaderText = "الاسم ";
+            gridViewTextBoxColumn4.Name = "PatName";
+            gridViewTextBoxColumn4.Width = 180;
+            gridViewTextBoxColumn5.EnableExpressionEditor = false;
+            gridViewTextBoxColumn5.FieldName = "InsuranceNo";
+            gridViewTextBoxColumn5.HeaderText = "رقم التامين";
+            gridViewTextBoxColumn5.Name = "InsuranceNo";
+            gridViewTextBoxColumn5.Width = 100;
+            gridViewTextBoxColumn6.EnableExpressionEditor = false;
+            gridViewTextBoxColumn6.FieldName = "VisitDate";
+            gridViewTextBoxColumn6.HeaderText = "تاريخ الزيارة";
+            gridViewTextBoxColumn6.Name = "VisitDate";
+            gridViewTextBoxColumn6.Width = 80;
+            gridViewTextBoxColumn7.EnableExpressionEditor = false;
+            gridViewTextBoxColumn7.FieldName = "VisitType";
+            gridViewTextBoxColumn7.HeaderText = "نوع الروشتة";
+            gridViewTextBoxColumn7.Name = "VisitType";
+            gridViewTextBoxColumn7.Width = 120;
+            gridViewTextBoxColumn8.EnableExpressionEditor = false;
+            gridViewTextBoxColumn8.FieldName = "GenericName";
+            gridViewTextBoxColumn8.HeaderText = "الاسم العلمي";
+            gridViewTextBoxColumn8.Name = "GenericName";
+            gridViewTextBoxColumn8.Width = 250;
+            gridViewTextBoxColumn9.EnableExpressionEditor = false;
+            gridViewTextBoxColumn9.FieldName = "TradeName";
+            gridViewTextBoxColumn9.HeaderText = "الاسم التجاري";
+            gridViewTextBoxColumn9.Name = "TradeName";
+            gridViewTextBoxColumn9.Width = 250;
+            gridViewTextBoxColumn10.EnableExpressionEditor = false;
+            gridViewTextBoxColumn10.FieldName = "Qty";
+            gridViewTextBoxColumn10.HeaderText = "الكمية";
+            gridViewTextBoxColumn10.Name = "Qty";
+            gridViewTextBoxColumn10.Width = 80;
+            gridViewTextBoxColumn11.EnableExpressionEditor = false;
+            gridViewTextBoxColumn11.FieldName = "UnitPrice";
+            gridViewTextBoxColumn11.HeaderText = "سعر الوحدة";
+            gridViewTextBoxColumn11.Name = "UnitPrice";
+            gridViewTextBoxColumn11.Width = 80;
+            gridViewTextBoxColumn12.FieldName = "PatPrice";
+            gridViewTextBoxColumn12.HeaderText = "المدفوع";
+            gridViewTextBoxColumn12.Name = "PatPrice";
+            gridViewTextBoxColumn12.Width = 80;
+            gridViewTextBoxColumn13.FieldName = "CLaimPrice";
+            gridViewTextBoxColumn13.HeaderText = "قيمة المطالبة";
+            gridViewTextBoxColumn13.Name = "CLaimPrice";
+            gridViewTextBoxColumn13.Width = 80;
+            gridViewTextBoxColumn14.EnableExpressionEditor = false;
+            gridViewTextBoxColumn14.FieldName = "TotalPrice";
+            gridViewTextBoxColumn14.HeaderText = "الجملة";
+            gridViewTextBoxColumn14.Name = "TotalPrice";
+            gridViewTextBoxColumn14.Width = 100;
+            this.radGridView2.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
+            gridViewTextBoxColumn1,
+            gridViewTextBoxColumn2,
+            gridViewTextBoxColumn3,
+            gridViewTextBoxColumn4,
+            gridViewTextBoxColumn5,
+            gridViewTextBoxColumn6,
+            gridViewTextBoxColumn7,
+            gridViewTextBoxColumn8,
+            gridViewTextBoxColumn9,
+            gridViewTextBoxColumn10,
+            gridViewTextBoxColumn11,
+            gridViewTextBoxColumn12,
+            gridViewTextBoxColumn13,
+            gridViewTextBoxColumn14});
+            this.radGridView2.MasterTemplate.EnableFiltering = true;
+            this.radGridView2.MasterTemplate.ViewDefinition = tableViewDefinition1;
+            this.radGridView2.Name = "radGridView2";
+            this.radGridView2.ReadOnly = true;
+            this.radGridView2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.radGridView2.Size = new System.Drawing.Size(1055, 579);
+            this.radGridView2.TabIndex = 2;
+            // 
+            // ClmsFilterCenterDataFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 579);
+            this.Controls.Add(this.radGridView2);
+            this.Name = "ClmsFilterCenterDataFrm";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "التحقق من بيانات المطالبة";
+            this.ThemeName = "Office2010Blue";
+            this.Load += new System.EventHandler(this.ClmsFilterCenterDataFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView2.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGridView radGridView2;
+    }
+}
---
+++ HealthServicesSystem/Claims/ClmsFilterCenterDataFrm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace HealthServicesSystem.Claims
+{
+    public partial class ClmsFilterCenterDataFrm : Telerik.WinControls.UI.RadForm
+    {
+        public ClmsFilterCenterDataFrm()
+        {
+            InitializeComponent();
+        }
+        public string Path = "";
+        private void ClmsFilterCenterDataFrm_Load(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(@"Provider= Microsoft.JET.OLEDB.4.0; Data Source ="+Path+";Persist Security Info =False;");
+            OleDbDataAdapter da = new OleDbDataAdapter("Select MasterTb.ID,MasterTb.FullName as PatName ,MasterTb.InsuranceNo ,MasterTb.Mnth as Months, MasterTb.yr as Years ,MasterTb.Age, MasterTb.Gender ,MasterTb.VisitNo , MasterTb.VisitDate, MasterTb.StateId, DetailsTb.TradeName, DetailsTb.Qty, DetailsTb.Price as UnitPrice, DetailsTb.Total as TotalPrice, DetailsTb.PatPrice, DetailsTb.ClaimPrice, Generics.GenericName , ContractType.ContractName as VisitType FROM ContractType INNER JOIN((Generics INNER JOIN DetailsTb ON Generics.GenericId = DetailsTb.GenericId) INNER JOIN MasterTb ON DetailsTb.MasterId = MasterTb.ID) ON ContractType.Id = MasterTb.TypeId ", con);
+                 // OleDbDataAdapter da = new OleDbDataAdapter("SELECT MasterTb.ID, MasterTb.InsuranceNo, MasterTb.FullName as PatName, MasterTb.Age, MasterTb.Gender, MasterTb.Mnth as Month, MasterTb.yr as Year, MasterTb.VisitNo , MasterTb.VisitDate, MasterTb.StateId, DetailsTb.TradeName, DetailsTb.Qty, DetailsTb.Price, DetailsTb.Total, DetailsTb.PatPrice, DetailsTb.ClaimPrice, Generics.GenericName FROM(Generics INNER JOIN DetailsTb ON Generics.GenericId = DetailsTb.GenericId) INNER JOIN MasterTb ON DetailsTb.MasterId = MasterTb.ID ", con);
+                 DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    radGridView2 .DataSource = dt;
+
+                }
+            }
+        
+    }
+}
